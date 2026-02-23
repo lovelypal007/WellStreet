c99758e908a09cf62aded3aac35000e9b232064e
lovelypal007 <lavishpalbsr96@gmail.com>
2026-02-23 13:46:37 +0530

    Fixed review comments.

--- js_unit_playwright-main/tests/steamTask.spec.ts
+++ js_unit_playwright-main/tests/steamTask.spec.ts
@@ -1,31 +1,36 @@
 import { expect, test } from "@playwright/test";
-import { SearchNavigate } from "../pageObjects/steamHomePage";
-import { SearchResults } from "../pageObjects/steamSearchPage";
-import { GamePage } from "../pageObjects/steamGamePage";
-import { AboutPage } from "../pageObjects/steamAboutPage";
+import { SteamHomePage } from "../pageObjects/steamHomePage";
+import { SteamSearchPage } from "../pageObjects/steamSearchPage";
+import { SteamGamePage } from "../pageObjects/steamGamePage";
+import { SteamAboutPage } from "../pageObjects/steamAboutPage";
 
-const testData = {
+const testData: { gameName:string,gameTitle:string,aboutPageTitle:string,indexNumber:number } = {
   gameName: "The Stanley Parable",
   gameTitle: "The Stanley Parable on Steam",
   aboutPageTitle: "Steam, The Ultimate Online Game Platform",
   indexNumber: 0
 }
 test('should navigate to Steam About page from the main page ', async ({ page }) => {
-  const steam = new SearchNavigate(page);
-  const searchList = new SearchResults(page);
-  const gamePage = new GamePage(page);
-  const aboutpage = new AboutPage(page);
+  const steam = new SteamHomePage(page);
+  const searchList = new SteamSearchPage(page);
+  const gamePage = new SteamGamePage(page);
+  const aboutPage = new SteamAboutPage(page);
+
   await steam.navigateToPage();
   await steam.searchForGame(testData.gameName);
+
   const firstResult = await searchList.getResultTitle(testData.indexNumber, testData.gameName);
-  await searchList.getResultTitle(testData.indexNumber, testData.gameName);
   await searchList.clickOnResult(testData.indexNumber);
-  await gamePage.gamePageIsDisplayed(testData.gameTitle);
+
+  await gamePage.verifyPageIsDisplayed(testData.gameTitle);
   const gameName = await gamePage.getGameName();
+
   expect(gameName).toBe(firstResult);
+
   await gamePage.clickDownloadButton();
   await gamePage.clickGotSteamButton();
-  await aboutpage.aboutPageIsDisplayed(testData.aboutPageTitle);
-  await aboutpage.clickInstallSteamButton();
-  await aboutpage.comparegamersCount();
+  
+  await aboutPage.verifyPageIsDisplayed(testData.aboutPageTitle);
+  await aboutPage.clickInstallSteamButton();
+  await aboutPage.compareGamersCount();
 });
