4ba7e02a8960718e2fcdfa4869b36e0e1ed6cd14
lovelypal007 <lavishpalbsr96@gmail.com>
2026-02-24 13:59:58 +0530

    Fix search results validation for multiple expected items.

--- js_unit_playwright-main/pageObjects/steamHomePage.ts
+++ js_unit_playwright-main/pageObjects/steamHomePage.ts
@@ -1,9 +1,11 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 
 export class SteamHomePage {
   public searchInput: Locator;
+  public searchDropdownResultList: Locator;
   constructor(public page: Page) {
     this.searchInput = page.locator('[type="text"]');
+    this.searchDropdownResultList = page.locator('[id^="searchSuggestions_"] a[href*="/app/"]');
   }
 
   public async navigateToPage() {
@@ -11,6 +13,13 @@ export class SteamHomePage {
   }
   public async searchForGame(gameName: string): Promise<void> {
     await this.searchInput.fill(gameName);
-    await this.searchInput.press('Enter');
   }
-}
+   async getResultsText(indexnumber:number): Promise<string> {
+    const results =(await this.searchDropdownResultList.nth(indexnumber).innerText()).split('\n');
+    console.log(results[indexnumber]);
+    return results[indexnumber];
+
+  }
+  async clickOnFirstResult(index: number): Promise<void> {
+    await this.searchDropdownResultList.nth(index).click();
+  }}
--- js_unit_playwright-main/tests/steamTask.spec.ts
+++ js_unit_playwright-main/tests/steamTask.spec.ts
@@ -1,36 +1,44 @@
 import { expect, test } from "@playwright/test";
 import { SteamHomePage } from "../pageObjects/steamHomePage";
-import { SteamSearchPage } from "../pageObjects/steamSearchPage";
+// import { SteamSearchPage } from "../pageObjects/steamSearchPage";
 import { SteamGamePage } from "../pageObjects/steamGamePage";
 import { SteamAboutPage } from "../pageObjects/steamAboutPage";
 
-const testData: { gameName:string,gameTitle:string,aboutPageTitle:string,indexNumber:number } = {
+
+const testData: { gameName:string,gameTitle:string,aboutPageTitle:string} = {
   gameName: "The Stanley Parable",
   gameTitle: "The Stanley Parable on Steam",
-  aboutPageTitle: "Steam, The Ultimate Online Game Platform",
-  indexNumber: 0
-}
+  aboutPageTitle: "Steam, The Ultimate Online Game Platform"
+};
+const testData2: {indexNumber:number,text:string }[] = [
+  {indexNumber: 0,text:"The Stanley Parable"},{indexNumber: 2,text:"The Stanley Parable Demo"}
+];
+
 test('should navigate to Steam About page from the main page ', async ({ page }) => {
   const steam = new SteamHomePage(page);
-  const searchList = new SteamSearchPage(page);
+  // const searchList = new SteamSearchPage(page);
   const gamePage = new SteamGamePage(page);
   const aboutPage = new SteamAboutPage(page);
 
   await steam.navigateToPage();
   await steam.searchForGame(testData.gameName);
 
-  const firstResult = await searchList.getResultTitle(testData.indexNumber, testData.gameName);
-  await searchList.clickOnResult(testData.indexNumber);
+    for(const item of testData2){
+      const resultText = await steam.getResultsText(item.indexNumber);
+      expect(resultText).toBe(item.text);
+    }
+  await steam.clickOnFirstResult(testData2[0].indexNumber);
 
   await gamePage.verifyPageIsDisplayed(testData.gameTitle);
-  const gameName = await gamePage.getGameName();
+  // const gameName = await gamePage.getGameName();
 
-  expect(gameName).toBe(firstResult);
+  // expect(gameName).toBe(firstResult);
 
   await gamePage.clickDownloadButton();
   await gamePage.clickGotSteamButton();
   
   await aboutPage.verifyPageIsDisplayed(testData.aboutPageTitle);
-  await aboutPage.clickInstallSteamButton();
+  await aboutPage.checkInsatallButtonIsClickable();
   await aboutPage.compareGamersCount();
+ 
 });
